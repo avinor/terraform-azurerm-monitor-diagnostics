ee6ba3a1bd64f52f8d23343d8be694f9a38dddea
Solem, Lars <lars.solem@avinor.no>
2020-03-31 14:34:39 +0200

    Upgrade to azurerm version 1.44 and fix error in output

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "id" {
   description = "The id of the diagnostic settings."
-  value       = azurerm_monitor_diagnostic_setting.main.id
+  value       = azurerm_monitor_diagnostic_setting.main[*].id
 }
